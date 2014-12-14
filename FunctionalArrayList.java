6cd7e3d5fc2cc5e3fd030a2d45f66e7de4e2f9c1
Paul Day <pday02@mail.bkk.ac.uk>
2014-12-14 23:46:39 +0000

    First commit

---
+++ FunctionalArrayList.java
@@ -0,0 +1,26 @@
+
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+
+	@Override
+	public ReturnObject head() {
+		
+		if (isEmpty()) return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		return new ReturnObjectImpl(get(0).getReturnValue());
+	}
+
+	@Override
+	public FunctionalList rest() {
+		
+		FunctionalList rest = new FunctionalArrayList();
+		
+		int i = 1;
+		while(!get(i).hasError())
+		{
+			rest.add(get(i).getReturnValue());
+			i++;
+		}
+		
+		return rest;
+	}
+
+}
