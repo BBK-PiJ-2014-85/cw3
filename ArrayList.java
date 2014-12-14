1a839fbc80053ce8af2ee6021490d8d568378171
Paul Day <pday02@mail.bkk.ac.uk>
2014-12-14 18:15:27 +0000

    Renamed to match spec

---
+++ ArrayList.java
@@ -0,0 +1,40 @@
+
+public class ArrayList implements List {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ReturnObject get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(int index, Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
