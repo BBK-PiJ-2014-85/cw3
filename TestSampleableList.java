a2c0ca65ef206010ac1fa4d04421d3e8d77ea8ec
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-11 00:16:44 +0000

    Added tests

--- TestSampleableList.java
+++ TestSampleableList.java
@@ -5,9 +5,49 @@ import org.junit.Test;
 
 public class TestSampleableList {
 
+
+	SampleableList testList = new SampleableListImpl();
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmpty() {
+		SampleableList emptyList = new SampleableListImpl();
+		SampleableList newList=emptyList.sample();
+		assertTrue(newList.isEmpty());
+	}
+	
+	@Test
+	public void  testSample(){
+
+		SampleableList list3 = new SampleableListImpl();
+		list3.add(1);
+		list3.add(2);
+		list3.add(3);
+		
+		SampleableList list3samp = list3.sample();
+		assertEquals(list3samp.size(), 2);
+		assertEquals(list3samp.get(0).getReturnValue(), 1);
+		assertEquals(list3samp.get(1).getReturnValue(), 3);
+		
+		SampleableList list4 = new SampleableListImpl();
+		list4.add(1);
+		list4.add(2);
+		list4.add(3);
+		
+		SampleableList list4samp = list4.sample();
+		assertEquals(list4samp.size(), 2);
+		assertEquals(list4samp.get(0).getReturnValue(), 1);
+		assertEquals(list4samp.get(1).getReturnValue(), 3);
+	}
+	
+	@Test
+	public void testOriginalListUnaltered(){
+		SampleableList list = new SampleableListImpl();
+		list.add(new String[] {"original","second"});
+		SampleableList listSample = list.sample();
+		String[] items = (String[]) listSample.get(0).getReturnValue();
+		items[0]="changed";
+		assertEquals(((String []) list.get(0).getReturnValue())[0], "original");
 	}
 
 }
