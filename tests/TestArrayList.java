c055dffe1d5678b430c324364f75642fb0a29ac9
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-27 21:32:10 +0000

    Moved file into tests package as per cw2 feedback. Use git log --follow to see full history.

---
+++ tests/TestArrayList.java
@@ -0,0 +1,19 @@
+
+public class TestArrayList extends TestList{
+
+	/*
+	 * Extended class TestList contains all the parameters and tests to be tested.
+	 * 
+	 * CreateList() as defined below ensures the tests being tested are as created by the class "ArrayList".
+	 */
+
+	public TestArrayList(int index, String name) {
+		super(index, name);
+	}
+
+	@Override
+	public List createStartingList() {
+		return new ArrayList();
+	}
+
+}
